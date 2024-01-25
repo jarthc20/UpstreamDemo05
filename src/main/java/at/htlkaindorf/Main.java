54997e83f12765740b7cd1c44ede8e7469dc69ff
jarthc20 <jarthc20@htl-kaindorf.at>
2024-01-25 13:30:21 +0100

    main

--- src/main/java/at/htlkaindorf/Main.java
+++ src/main/java/at/htlkaindorf/Main.java
@@ -3,5 +3,7 @@ package at.htlkaindorf;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("EMMA IST DOOF");
+        System.out.println("JETZT IST DIESE COOOOLE US FERTIG, OBWOHL SIE NICHT DER WAHRHEIT ENTSPRICHT");
     }
 }
