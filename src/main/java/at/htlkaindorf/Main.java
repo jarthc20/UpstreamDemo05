229a0d61b0b119ef4c6d906445f0f32862cf4be1
bauer <bauemc20@htl-kaindorf.at>
2024-01-25 13:33:04 +0100

    working on issue1

--- src/main/java/at/htlkaindorf/Main.java
+++ src/main/java/at/htlkaindorf/Main.java
@@ -3,5 +3,10 @@ package at.htlkaindorf;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello my friends");
+        System.out.println("How are you today");
+        System.out.println("Thomas es muy gordo y feo");
+        System.out.println("Made in US1");
     }
+
 }
